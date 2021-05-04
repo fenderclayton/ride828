c0777572b86d8063c1761e340770bb4378bd19f8
Clayton <clayton@Claytons-Mac-mini.local>
2021-05-04 01:30:50 -0400

    First commit

---
+++ github/ride828/App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+
+import DestinationSearch from './src/screens/DestinationSearch/index.js';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+     <StatusBar barStyle="dark-content" />
+     <DestinationSearch />
+    </> 
+  );
+};
+
+export default App;
